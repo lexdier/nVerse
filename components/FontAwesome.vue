335cff9e7cc9964d80a8021be4236a04d63d1397
lexdier <saviolexdier@gmail.com>
2024-03-15 17:17:49 -0400

    feat: create font awesome component for icons

---
+++ components/FontAwesome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { PropType, computed } from 'vue';
+
+const $props = defineProps({
+  name: { type: String, required: true },
+  animation: { type: String, required: false },
+  variant: { type: String, required: false },
+  mode: { type: String as PropType<'solid' | 'regular' | 'duotone' | 'brands'>, default: 'regular' },
+})
+
+const classes = computed(() => [
+  `fa-${$props.animation}`,
+  `fa-${$props.name}`,
+  `fa-${$props.mode}`,
+  `text-${$props.variant}`,
+].join(' '))
+</script>
+
+<template>
+  <span :class="classes"/>
+</template>
