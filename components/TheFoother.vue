30ee10c76d30fa6ed9adaf40f0ee325551428c33
lexdier <saviolexdier@gmail.com>
2024-03-15 14:57:22 -0400

    feat: create footer component

---
+++ components/TheFoother.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="bg-primary d-flex">
+        <BContainer class="py-2">
+            <BRow class="d-flex align-items-center w-100" align-h="end"> 
+                <BCol cols="12" class="text-center">
+                    <p>"As good minimalists, we have reduced this page to nothing. Another try?”</p>
+                </BCol>
+
+                <BCol cols="3">
+                   <span>404</span>
+                </BCol>
+            </BRow>
+        </BContainer>
+    </section>
+</template>
