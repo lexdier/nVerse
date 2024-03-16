0f70b0d548b101bd5131fc7d2e52c25c292f36a9
lexdier <saviolexdier@gmail.com>
2024-03-16 16:37:39 -0400

    refactor: rename componet

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ import BlogSection from '~/components/BlogSection.vue';
 import CarouselSection from '~/components/CarouselSection.vue';
 import PresentationSection from '~/components/PresentationSection.vue';
 import SectionContact from '~/components/SectionContact.vue';
-import SectionPricing from '~/components/SectionPricing.vue';
+import PricingSection from '~/components/PricingSection.vue';
 import ServicesSection from '~/components/ServicesSection.vue';
 
 </script>
@@ -17,7 +17,7 @@ import ServicesSection from '~/components/ServicesSection.vue';
 
       <SectionContact/>
 
-      <SectionPricing/>
+      <PricingSection/>
 
       <CarouselSection/>
 
