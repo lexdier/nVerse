85274ad769c7fd3ac9dc0e4220ee905dd120a5ca
lexdier <saviolexdier@gmail.com>
2024-03-16 16:34:05 -0400

    feat: add new component to app

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import BlogSection from '~/components/BlogSection.vue';
 import CarouselSection from '~/components/CarouselSection.vue';
+import PresentationSection from '~/components/PresentationSection.vue';
 import SectionContact from '~/components/SectionContact.vue';
 import SectionPricing from '~/components/SectionPricing.vue';
+import ServicesSection from '~/components/ServicesSection.vue';
 
 </script>
 
 <template>
   <main>
     <BContainer>
+      <PresentationSection/>
+
+      <ServicesSection/>
+
       <SectionContact/>
 
       <SectionPricing/>
